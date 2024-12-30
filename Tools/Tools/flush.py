7def4a2d5e3076bbeb32121d20034f4b8dc2f7b2
LightingXT <leoverysmallno10@gmail.com>
2024-12-30 19:46:42 +0800

    Update flush.py

--- Tools/Tools/flush.py
+++ Tools/Tools/flush.py
@@ -4,11 +4,12 @@ class flush:
     @classmethod
     def __init__(self):
         self.goflush = False
+        @classmethod
     def init(self):
         self.goflush = True
+        @classmethod
     def flush(self,name):
         if self.goflush:False
         print("pls run flush.init.")
-
         sys.stdout.write(name)
         sys.stdout.flush()
